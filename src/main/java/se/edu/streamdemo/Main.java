69f51e1b9baa357ab447d4fb8a1394a2ade11a06
Jiaming <shenjiaming27@gmail.com>
2024-10-04 11:06:06 +0800

    updated methods using streams

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -12,13 +12,15 @@ public class Main {
         Datamanager dataManager = new Datamanager("./data/data.txt");
         ArrayList<Task> tasksData = dataManager.loadData();
 
-        System.out.println("Printing all data ...");
-        printAllData(tasksData);
+//        System.out.println("Printing all data ...");
+//        printAllData(tasksData);
+//        printDataWithStreams(tasksData);
 
-        System.out.println("Printing deadlines ...");
-        printDeadlines(tasksData);
-
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+//        System.out.println("Printing deadlines ...");
+//        printDeadlines(tasksData);
+//        printDeadlinesUsingStreams(tasksData);
+        System.out.println("number of deadlines using iteration: " + countDeadlines(tasksData));
+        System.out.println("number of deadlines using streams: " + countDeadlinesUsingStreams(tasksData));
 
     }
 
